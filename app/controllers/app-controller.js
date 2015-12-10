2b3f560a9e63889862185ebeb0301a1d8bebdb1e
Solms Dykman <solmsd@gmail.com>
2015-12-10 11:19:01 +0900

    Start btn generates random btn; Set up sequence; Set up the start of isActive()

--- app/controllers/app-controller.js
+++ app/controllers/app-controller.js
@@ -1,6 +1,29 @@
-var appCtrl = angular.module('simonApp', []);
+var appCtrl = angular.module('simonApp', []),
+    buttons = ['TL', 'TR', 'BL', 'BR'],
+    real_sequence = [],
+    user_sequence = [],
+    active = '';
+
 appCtrl.controller('SimonBtnController', function(){
+  // Let the games begin!
+  this.start = function(){
+    real_sequence.push(buttons[Math.floor(Math.random()*4)]);
+    flashSequence();
+  }
+
+  this.isActive = function(div){
+    return active == div;
+  }
+
+  // Track the buttons the user pressed
   this.clicked = function(who){
-    alert(who);
+    active = who;
   }
 });
+
+// Show the user the real sequence
+function flashSequence(){
+  real_sequence.forEach(function(step){
+    alert(step);
+  });
+}
