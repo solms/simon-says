577412c31a4a1860f6bd06133a1a9d2644a8dc15
Solms Dykman <solmsd@gmail.com>
2015-12-10 18:55:37 +0900

    Got the lights flashing with !

--- app/controllers/app-controller.js
+++ app/controllers/app-controller.js
@@ -5,7 +5,7 @@ var appCtrl = angular.module('simonApp', []),
     active = '',
     turn = 'AI';
 
-appCtrl.controller('SimonBtnController', function($timeout){
+appCtrl.controller('SimonBtnController', function($interval){
   // Let the games begin!
   this.start = function(){
     real_sequence.push(buttons[Math.floor(Math.random()*4)]);
@@ -13,12 +13,29 @@ appCtrl.controller('SimonBtnController', function($timeout){
   }
 
   var flashSequence = function(){
-    real_sequence.forEach(function(step){
+    var i = 0;
+    $interval(function(){
+      if(active == ''){
+        active = real_sequence[i];
+        i++;
+      } else {
+        active = '';
+      }
+    }, 500, real_sequence.length*2);
+    /*for(var i=0; i<real_sequence.length; i++){
+      console.log('i:'+i);
+      active = real_sequence[i];
+      $timeout(function(){
+        active = '';
+      }, 1000);
+    }*/
+    /*real_sequence.forEach(function(step){
         active = step;
         $timeout(function () {
           active = '';
         }, 1000);
-    });
+    });*/
+    turn = 'PLAYER';
   }
 
   this.isActive = function(div){
@@ -26,9 +43,23 @@ appCtrl.controller('SimonBtnController', function($timeout){
   }
 
   // Track the buttons the user pressed
-  this.clicked = function(who){
-    if(turn == 'PLAYER'){
-        active = who;
+  this.clicked = function(btn){
+
+    if(turn == 'PLAYER' && user_sequence.length<real_sequence.length){
+      user_sequence.push(btn);
+    }
+    if(turn == 'PLAYER' && user_sequence.length == real_sequence.length){
+      if(user_sequence.toString() == real_sequence.toString()){
+        console.log('Nice work!');
+        turn = 'AI';
+        user_sequence = [];
+        real_sequence.push(buttons[Math.floor(Math.random()*4)]);
+        flashSequence();
+      } else {
+        real_sequence = [];
+        user_sequence = [];
+        turn = 'AI';
+      }
     }
   }
 });
