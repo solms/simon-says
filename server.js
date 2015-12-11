38de18fd81884b6eae780a29ab6c1bcc3ba9beec
Solms Dykman <solmsd@gmail.com>
2015-12-11 14:22:59 +0900

    Updated port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 routes(app);
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Node.js listening on port ' + port + '...');
 });
